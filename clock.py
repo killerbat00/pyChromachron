2dff7099e1a044e8c2d7c129756e89868936b9a9
brian morrow <bmorrow92@gmail.com>
2013-09-29 02:29:47 -0400

    Converted clock objects to sprites and began using pygame's builtin Sprite classes and methods.  Also updated script to start clock & show the current time

--- clock.py
+++ clock.py
@@ -8,51 +8,30 @@ inspired by awesome german design and pixies
 '''
 import pygame, sys, random, time, math, pygame.gfxdraw
 from pygame.locals import *
+from datetime import datetime
 
-fps = 30
+fps = 1
 winwidth = 500
 winheight = 500
-colors = {
-    0 : (255,235,35),
-    1 : (255,125,25),
-    2 : (255,195,195),
-    3 : (215,35,45),
-    4 : (255,105,205),
-    5 : (185,25,165),
-    6 : (65,65,184),
-    7 : (45,125,45),
-    8 : (5,195,195),
-    9 : (155,75,55),
-    10: (225,215,185),
-    11: (205,155,105)
-}
-graycolors = {
-    'gray' : (204,204,204),
-    'slightlygrayergray' : (102,102,102)
-}
-
-def degToRad(deg):
-    return deg/180.0 * math.pi
-def radToDeg(rad):
-    return 180.0/math.pi * rad
 
 def main():
-    global fpsclock
     pygame.init()
     fpsclock = pygame.time.Clock()
     display = pygame.display.set_mode((winwidth, winheight))
     pygame.display.set_caption('chromachron')
-    cf = ClockFace(color='color')
-    cf.draw()
-    while True:
-        checkForQuit()
-        clock()
+    pygame.mouse.set_visible(0)
 
-def clock():
+    cf = ClockFace()
     ch = ClockHands()
-    ch.draw()
-    pygame.display.update()
-    fpsclock.tick(fps)
+    ch.setInitTime()
+    sprites = pygame.sprite.RenderPlain((cf, ch))
+
+    while True:
+        fpsclock.tick(fps)
+        sprites.update()
+        sprites.draw(display)
+        pygame.display.flip()
+        checkForQuit()
 
 def terminate():
     pygame.quit()
@@ -65,47 +44,42 @@ def checkForQuit():
         if event.key == K_ESCAPE:
             terminate()
 
-class ClockHands:
+class ClockHands(pygame.sprite.Sprite):
     def __init__(self):
-        pass
-    def updatePos(self):
-        pass
-    def draw(self):
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('ch.png')
+        self.original = self.image
+        self.rect = self.image.get_rect()
+        self.angle = 0
 
-class ClockFace:
-    def __init__(self, color=None):
-        self.surf = pygame.display.get_surface()
-        self.center = (winwidth/2, winheight/2)
-        self.radius = 200
-        self.pieces = self.makePieces()
-        if color != None:
-            self.bg = 'color'
+    # magic
+    def setInitTime(self):
+        def _getTime():
+            time = datetime.now().time()
+            return str(time)[:5]
+
+        time = _getTime()
+        hour = time[:2]
+        min = time[3:]
+        hDeg = int(hour)*30
+        mDeg = int(min)/2
+        self.angle = (hDeg+mDeg)*-1
+
+    def update(self):
+        center = self.rect.center
+        self.angle = self.angle - 0.008335
+        if self.angle == -360:
+            self.angle = 0
+            self.image = self.original
         else:
-            self.bg = 'gray'
+            self.image = pygame.transform.rotate(self.original, self.angle)
+        self.rect = self.image.get_rect(center=center)
 
-    def makePieces(self):
-        ''' pieces = [(Surface, None, Rect, start_angle, stop_angle, None)]'''
-        pieces = []
-        for x in range(12):
-            start = 30*x
-            end = start+30
-            p = (self.surf, self.center[0], self.center[1], self.radius, start, end, None)
-            pieces.append(p)
-        return pieces
-
-    def draw(self):
-        def _colorize(pieces, bg):
-            if bg == 'color':
-                for x in range(12):
-                    pieces[x] = list(pieces[x])
-                    pieces[x][6] = colors[x]
-            elif bg == 'gray':
-                pass
-            return pieces
-        self.pieces = _colorize(self.pieces, self.bg)
-        for piece in self.pieces:
-            pygame.gfxdraw.pie(piece[0], piece[1], piece[2], piece[3], piece[4], piece[5], piece[6])
+class ClockFace(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('bg.png')
+        self.rect = self.image.get_rect()
 
 if __name__ == '__main__':
     main()
